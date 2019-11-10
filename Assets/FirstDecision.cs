6633b393fdb19a52089cebe32982b514e9adb493
moustafaatef74 <myemail>
2019-11-10 04:34:14 +0200

    2nd decision

---
+++ Assets/FirstDecision.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class FirstDecision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var timelineAsset = this.gameObject.GetComponent<PlayableDirector>().playableAsset as TimelineAsset;
+        var animationtrack = timelineAsset.GetOutputTrack(1) as AnimationTrack;
+        IEnumerable clips = animationtrack.GetClips();
+        var clip = clips.GetEnumerator();
+        print(clip);
+        
+    }
+}
