569c1ac7307157bd0827ff2809f09dcb67e1c28f
moustafaatef74 <myemail>
2019-11-10 19:57:22 +0200

    commit

--- Assets/FirstDecision.cs
+++ Assets/FirstDecision.cs
@@ -3,22 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
 using UnityEngine.Timeline;
+using UnityEngine.UI;
 
 public class FirstDecision : MonoBehaviour
 {
+    public GameObject FirstCanvas;
+    public GameObject DecisionOne;
+    public GameObject DecisionTwo;
+    public GameObject DecisionTwoAnim;
+    bool Select = true;
     // Start is called before the first frame update
     void Start()
     {
+        DecisionOne.GetComponent<Button>().onClick.AddListener(() => {
+            this.gameObject.GetComponent<PlayableDirector>().Play();
+        });
+        DecisionTwo.GetComponent<Button>().onClick.AddListener(() => {
+            this.gameObject.GetComponent<PlayableDirector>().Play();
+        });
     }
 
     // Update is called once per frame
     void Update()
     {
-        var timelineAsset = this.gameObject.GetComponent<PlayableDirector>().playableAsset as TimelineAsset;
-        var animationtrack = timelineAsset.GetOutputTrack(1) as AnimationTrack;
-        IEnumerable clips = animationtrack.GetClips();
-        var clip = clips.GetEnumerator();
-        print(clip);
-        
     }
+
 }
